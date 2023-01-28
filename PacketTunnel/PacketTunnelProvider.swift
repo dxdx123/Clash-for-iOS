7fac8688c436032259a79bd18c764cbd5bd118c5
Arror <hallo.maqiang@icloud.com>
2023-01-28 11:13:49 +0800

    Set mtu 9000

--- PacketTunnel/PacketTunnelProvider.swift
+++ PacketTunnel/PacketTunnelProvider.swift
@@ -4,7 +4,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
     
     override func startTunnel(options: [String : NSObject]? = nil) async throws {
         let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "254.1.1.1")
-        settings.mtu = 1500
+        settings.mtu = 9000
         settings.ipv4Settings = {
             let settings = NEIPv4Settings(addresses: ["198.18.0.1"], subnetMasks: ["255.255.0.0"])
             settings.includedRoutes = [NEIPv4Route.default()]
