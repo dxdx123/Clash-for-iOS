0e569ac70b8626b5393f393b1d5a788befe88ae6
Arror <hallo.maqiang@icloud.com>
2023-02-28 15:23:14 +0800

    Update Project

--- XrayTunnel/PacketTunnelProvider.swift
+++ XrayTunnel/PacketTunnelProvider.swift
@@ -1,11 +1,11 @@
 import NetworkExtension
 import XrayKit
 
-@_silgen_name("start_tun2socks") private func tun2socks(_ config: UnsafePointer<CChar>!)
+@_silgen_name("start_tun2socks") private func tun2socks(_ fd: Int32, _ config: UnsafePointer<CChar>!)
 
-@frozen enum Tun2Socks {
+class PacketTunnelProvider: NEPacketTunnelProvider {
     
-    private static var tunnelFileDescriptor: Int32? {
+    private var tunnelFileDescriptor: Int32? {
         var buf = Array<CChar>(repeating: 0, count: Int(IFNAMSIZ))
         return (1...1024).first {
             var len = socklen_t(buf.count)
@@ -13,39 +13,15 @@ import XrayKit
         }
     }
     
-    static func run(port: Int) {
-        guard let fd = tunnelFileDescriptor else {
-            fatalError("Get tunnel file descriptor failed.")
-        }
-        DispatchQueue.global(qos: .userInitiated).async {
-            let config: String = """
-            [General]
-            always-real-ip = *
-            tun-fd = \(fd)
-
-            [Proxy]
-            XRAY = socks, 127.0.0.1, \(port)
-
-            [Rule]
-            FINAL, XRAY
-            """
-            tun2socks(config.cString(using: .utf8))
-        }
-    }
-}
-
-
-class PacketTunnelProvider: NEPacketTunnelProvider {
-    
     override func startTunnel(options: [String : NSObject]? = nil) async throws {
         let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "254.1.1.1")
-        settings.mtu = 1500
+        settings.mtu = 9000
         settings.ipv4Settings = {
             let settings = NEIPv4Settings(addresses: ["198.18.0.1"], subnetMasks: ["255.255.0.0"])
             settings.includedRoutes = [NEIPv4Route.default()]
             return settings
         }()
-        settings.dnsSettings = NEDNSSettings(servers: ["114.114.114.114"])
+        settings.dnsSettings = NEDNSSettings(servers: ["8.8.8.8", "114.114.114.114"])
         try await self.setTunnelNetworkSettings(settings)
         do {
             var error: NSError? = nil
@@ -98,8 +74,8 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                 },
                 "inbounds": [
                     {
-                        "listen": "127.0.0.1",
-                        "port": 9090,
+                        "listen": "[::1]",
+                        "port": 7890,
                         "protocol": "socks",
                         "settings": {
                             "udp": true
@@ -115,34 +91,6 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                 ],
                 "outbounds": [
                     {
-                        "protocol": "vless",
-                        "settings": {
-                            "vnext": [
-                                {
-                                    "address": "www.arror.org",
-                                    "port": 443,
-                                    "users": [
-                                        {
-                                            "id": "0c83e70f-9f49-40dc-b05b-0c45fb0a12c6",
-                                            "encryption": "none",
-                                            "flow": "xtls-rprx-vision"
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        "streamSettings": {
-                            "network": "tcp",
-                            "security": "tls",
-                            "tlsSettings": {
-                                "serverName": "www.arror.org",
-                                "allowInsecure": false,
-                                "fingerprint": "chrome"
-                            }
-                        },
-                        "tag": "proxy"
-                    },
-                    {
                         "protocol": "freedom",
                         "tag": "direct"
                     },
@@ -155,8 +103,14 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
             """
             XrayRun(json, self, &error)
             try error.flatMap { throw $0 }
-            DispatchQueue.main.async {
-                Tun2Socks.run(port: 9090)
+            DispatchQueue.global(qos: .userInitiated).async {
+                guard let fd = self.tunnelFileDescriptor else {
+                    fatalError()
+                }
+                guard let file = Bundle.main.path(forResource: "main", ofType: "yml") else {
+                    fatalError()
+                }
+                tun2socks(fd, file)
             }
         } catch {
             NSLog("--------> \(error.localizedDescription)")
@@ -164,25 +118,17 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         }
     }
     
-    private var tunnelFileDescriptor: Int32? {
-        var buf = Array<CChar>(repeating: 0, count: Int(IFNAMSIZ))
-        return (1...1024).first {
-            var len = socklen_t(buf.count)
-            return getsockopt($0, 2, 2, &buf, &len) == 0 && String(cString: buf).hasPrefix("utun")
-        }
-    }
-    
-    override func stopTunnel(with reason: NEProviderStopReason) async {
-        
-    }
-    
-    override func handleAppMessage(_ messageData: Data) async -> Data? {
-        return nil
-    }
+//    override func stopTunnel(with reason: NEProviderStopReason) async {
+//
+//    }
+//
+//    override func handleAppMessage(_ messageData: Data) async -> Data? {
+//        return nil
+//    }
 }
 
 extension PacketTunnelProvider: XrayLoggerProtocol {
-    
+
     func onLog(_ msg: String?) {
         msg.flatMap { NSLog($0) }
     }
