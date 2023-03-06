1c58d4c4239b356f7c0483fa75030dfd406e1bd7
Arror <hallo.maqiang@icloud.com>
2023-03-06 17:52:09 +0800

    Add log hijack

--- XrayTunnel/PacketTunnelProvider.swift
+++ XrayTunnel/PacketTunnelProvider.swift
@@ -2,7 +2,9 @@ import NetworkExtension
 import XrayKit
 import TunnelKit
 
-class PacketTunnelProvider: MGPacketTunnelProvider, XrayLoggerProtocol {
+class PacketTunnelProvider: MGPacketTunnelProvider {
+    
+    let logger = MangoLogger(subsystem: "ABCD", category: "KSDJFLKSF")
     
     private var logLevel: MGLogLevel {
         MGLogLevel(rawValue: UserDefaults.shared.string(forKey: MGConstant.logLevel) ?? "") ?? .silent
@@ -18,12 +20,47 @@ class PacketTunnelProvider: MGPacketTunnelProvider, XrayLoggerProtocol {
         config.override(log: self.logLevel)
         config.override(inbound: port)
         var error: NSError? = nil
-        XrayRun(try config.asJSONString(), self, &error)
+        XrayRun(try config.asJSONString(), &error)
         try error.flatMap { throw $0 }
         try Tunnel.start(port: port)        
     }
+}
 
-    func onLog(_ msg: String?) {
-        msg.flatMap { NSLog($0) }
+
+import os
+
+public final class MangoLogger {
+    
+    private let logger: Logger
+    
+    public init(subsystem: String, category: String) {
+        self.logger = Logger(subsystem: subsystem, category: category)
+        self.hijack(fileDescriptor: FileHandle.standardOutput.fileDescriptor)
+        self.hijack(fileDescriptor: FileHandle.standardError.fileDescriptor)
+    }
+    
+    public func log(_ message: String) {
+        self.logger.log("\(message, privacy: .public)")
+    }
+    
+    private func hijack(fileDescriptor: Int32) {
+        var rw = Array<Int32>(repeating: 0, count: 2)
+        setbuf(fdopen(fileDescriptor, "w"), nil)
+        pipe(&rw)
+        dup2(rw[1], fileDescriptor)
+        let fileHandle = FileHandle(fileDescriptor: rw[0])
+        fileHandle.waitForDataInBackgroundAndNotify()
+        fileHandle.readabilityHandler = { handler in
+            let data = handler.availableData
+            guard !data.isEmpty, let string = String(data: data, encoding: .utf8) else {
+                return
+            }
+            string.components(separatedBy: CharacterSet(arrayLiteral: "\n")).forEach { value in
+                guard !value.isEmpty else {
+                    return
+                }
+                self.log(value)
+            }
+        }
     }
 }
