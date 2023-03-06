49dced25c361d8a54ee327f956e64c47e2fb51f6
Arror <hallo.maqiang@icloud.com>
2023-03-06 11:55:56 +0800

    Update Tun2SocksKit

--- TunnelKit/Tunnel.swift
+++ TunnelKit/Tunnel.swift
@@ -3,63 +3,6 @@ import Tun2SocksKit
 
 public enum Tunnel {
     
-    private static var tunnelFileDescriptor: Int32? {
-        var ctlInfo = ctl_info()
-        withUnsafeMutablePointer(to: &ctlInfo.ctl_name) {
-            $0.withMemoryRebound(to: CChar.self, capacity: MemoryLayout.size(ofValue: $0.pointee)) {
-                _ = strcpy($0, "com.apple.net.utun_control")
-            }
-        }
-        for fd: Int32 in 0...1024 {
-            var addr = sockaddr_ctl()
-            var ret: Int32 = -1
-            var len = socklen_t(MemoryLayout.size(ofValue: addr))
-            withUnsafeMutablePointer(to: &addr) {
-                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
-                    ret = getpeername(fd, $0, &len)
-                }
-            }
-            if ret != 0 || addr.sc_family != AF_SYSTEM {
-                continue
-            }
-            if ctlInfo.ctl_id == 0 {
-                ret = ioctl(fd, CTLIOCGINFO, &ctlInfo)
-                if ret != 0 {
-                    continue
-                }
-            }
-            if addr.sc_id == ctlInfo.ctl_id {
-                return fd
-            }
-        }
-        return nil
-    }
-    
-    private static var interfaceName: String? {
-        guard let tunnelFileDescriptor = self.tunnelFileDescriptor else {
-            return nil
-        }
-        var buffer = [UInt8](repeating: 0, count: Int(IFNAMSIZ))
-        return buffer.withUnsafeMutableBufferPointer { mutableBufferPointer in
-            guard let baseAddress = mutableBufferPointer.baseAddress else {
-                return nil
-            }
-            var ifnameSize = socklen_t(IFNAMSIZ)
-            let result = getsockopt(
-                tunnelFileDescriptor,
-                2 /* SYSPROTO_CONTROL */,
-                2 /* UTUN_OPT_IFNAME */,
-                baseAddress,
-                &ifnameSize
-            )
-            if result == 0 {
-                return String(cString: baseAddress)
-            } else {
-                return nil
-            }
-        }
-    }
-    
     public static func start(port: Int) throws {
         let config = """
         tunnel:
@@ -82,11 +25,8 @@ public enum Tunnel {
         let file = cache.appending(component: "\(UUID().uuidString).yml", directoryHint: .notDirectory)
         try config.write(to: file, atomically: true, encoding: .utf8)
         DispatchQueue.global(qos: .userInitiated).async {
-            guard let fd = self.tunnelFileDescriptor else {
-                fatalError()
-            }
             DispatchQueue.global(qos: .userInitiated).async {
-                NSLog("HEV_SOCKS5_TUNNEL_MAIN: \(Socks5Tunnel.run(withFileDescriptor: fd, configFilePath: file.path(percentEncoded: false)))")
+                NSLog("HEV_SOCKS5_TUNNEL_MAIN: \(Socks5Tunnel.run(withConfig: file.path(percentEncoded: false)))")
             }
         }
     }
